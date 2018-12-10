10a521098040d07688c328954d06e978307bb489
aalom <44163993+aalom@users.noreply.github.com>
2018-12-10 22:05:16 +0600

    Added useful comments
    Comments indicates what the statements do and  why they have been used that way. Also, an error has been fixed and this has been also commented.

--- my-Codes/sumofDigitsofaNumber.py
+++ my-Codes/sumofDigitsofaNumber.py
@@ -1,7 +1,10 @@
+#this line takes input as int showing "Enter a number" string to the user
 n=int(input("Enter a number:"))
 tot=0
 while(n>0):
+#the lines inside the while loop are shown using indentation
     dig=n%10
     tot=tot+dig
-    n=n/10
+    n=n//10 #Previously this line had n/10. This resulted error. This has been fixed
+#this line will print the total of the digits of the given number
 print("The total sum of digits is:",tot)
